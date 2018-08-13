93f57db0c74fd109e53ed7ce8319dd5b0e9ad9d8
Sergio Flores <relfos@gmail.com>
2018-08-13 16:10:00 +0100

    Added Wallet.js

---
+++ www/public/js/wallet.js
@@ -0,0 +1,13 @@
+function copyText(text, name) {
+
+	navigator.clipboard.writeText(text).then(function() {
+		bootbox.alert(name+" was copied to the clipboard.");		
+	}, function(err) {
+		bootbox.alert("Could not copy "+name+"...");
+  });
+  
+/*  var copyText = document.getElementById("myAddress");
+  copyText.select();
+  document.execCommand("copy");
+*/  
+}
