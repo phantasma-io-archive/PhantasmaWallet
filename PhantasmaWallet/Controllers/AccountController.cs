7f91affc08f11688f9cbafcad51cf077c8353ad3
Bruno Freitas <bruno_freitas_2015@outlook.com>
2018-12-12 12:18:10 +0000

    refactor

--- PhantasmaWallet/Controllers/AccountController.cs
+++ PhantasmaWallet/Controllers/AccountController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Phantasma.Blockchain;
-using Phantasma.Blockchain.Contracts;
-using Phantasma.Blockchain.Contracts.Native;
 using Phantasma.Blockchain.Tokens;
 using Phantasma.Core.Types;
 using Phantasma.Cryptography;
@@ -109,129 +107,12 @@ namespace Phantasma.Wallet.Controllers
                 {
                     Date = new Timestamp(tx.Timestamp),
                     Hash = tx.Txid,
-                    Description = GetTxDescription(tx)
+                    Description = Utils.GetTxDescription(tx, PhantasmaChains, PhantasmaTokens)
                 });
             }
 
             return txs.ToArray();
         }
-
-        private string GetTxDescription(AccountTx tx)
-        {
-            string description = null;
-
-            string senderToken = null;
-            Address senderChain = Address.FromText(tx.ChainAddress);
-            Address senderAddress = Address.Null;
-
-            string receiverToken = null;
-            Address receiverChain = Address.Null;
-            Address receiverAddress = Address.Null;
-
-            BigInteger amount = 0;
-
-            foreach (var evt in tx.Events) //todo move this
-            {
-                Blockchain.Contracts.Event nativeEvent;
-                if (evt.Data != null)
-                {
-                    nativeEvent = new Blockchain.Contracts.Event((EventKind)evt.EvtKind,
-                        Address.FromText(evt.EventAddress), evt.Data.Decode());
-                }
-                else
-                {
-                    nativeEvent =
-                        new Blockchain.Contracts.Event((EventKind)evt.EvtKind, Address.FromText(evt.EventAddress));
-                }
-
-                switch (evt.EvtKind)
-                {
-                    case EvtKind.TokenSend:
-                        {
-                            var data = nativeEvent.GetContent<TokenEventData>();
-                            amount = data.value;
-                            senderAddress = nativeEvent.Address;
-                            senderToken = (data.symbol);
-                        }
-                        break;
-
-                    case EvtKind.TokenReceive:
-                        {
-                            var data = nativeEvent.GetContent<TokenEventData>();
-                            amount = data.value;
-                            receiverAddress = nativeEvent.Address;
-                            receiverChain = data.chainAddress;
-                            receiverToken = data.symbol;
-                        }
-                        break;
-
-                    case EvtKind.TokenEscrow:
-                        {
-                            var data = nativeEvent.GetContent<TokenEventData>();
-                            amount = data.value;
-                            var amountDecimal = TokenUtils.ToDecimal(amount,
-                                PhantasmaTokens.SingleOrDefault(p => p.Symbol == data.symbol).Decimals);
-                            receiverAddress = nativeEvent.Address;
-                            receiverChain = data.chainAddress;
-                            var chain = GetChainName(receiverChain.Text);
-                            description =
-                                $"{amountDecimal} {data.symbol} tokens escrowed for address {receiverAddress} in {chain}";
-                        }
-                        break;
-                    case EvtKind.AddressRegister:
-                        {
-                            var name = nativeEvent.GetContent<string>();
-                            description = $"{nativeEvent.Address} registered the name '{name}'";
-                        }
-                        break;
-
-                    case EvtKind.FriendAdd:
-                        {
-                            var address = nativeEvent.GetContent<Address>();
-                            description = $"{nativeEvent.Address} added '{address} to friends.'";
-                        }
-                        break;
-
-                    case EvtKind.FriendRemove:
-                        {
-                            var address = nativeEvent.GetContent<Address>();
-                            description = $"{nativeEvent.Address} removed '{address} from friends.'";
-                        }
-                        break;
-                }
-            }
-
-            if (description == null)
-            {
-                if (amount > 0 && senderAddress != Address.Null && receiverAddress != Address.Null &&
-                    senderToken != null && senderToken == receiverToken)
-                {
-                    var amountDecimal = TokenUtils.ToDecimal(amount,
-                        PhantasmaTokens.SingleOrDefault(p => p.Symbol == senderToken).Decimals);
-                    description =
-                        $"{amountDecimal} {senderToken} sent from {senderAddress.Text} to {receiverAddress.Text}";
-                }
-                else if (amount > 0 && receiverAddress != Address.Null && receiverToken != null)
-                {
-                    var amountDecimal = TokenUtils.ToDecimal(amount,
-                        PhantasmaTokens.SingleOrDefault(p => p.Symbol == receiverToken).Decimals);
-                    description = $"{amountDecimal} {receiverToken} received on {receiverAddress.Text} ";
-                }
-                else
-                {
-                    description = "Custom transaction";
-                }
-
-                if (receiverChain != Address.Null && receiverChain != senderChain)
-                {
-                    description +=
-                        $" from {GetChainName(senderChain.Text)} chain to {GetChainName(receiverChain.Text)} chain";
-                }
-            }
-
-            return description;
-        }
-
 
         public async Task<SendRawTx> SettleBlockTransfer(KeyPair keyPair, string sourceChainAddress, string blockHash,
             string destinationChainAddress)
@@ -354,16 +235,6 @@ namespace Phantasma.Wallet.Controllers
             }
         }
 
-        private string GetChainName(string address)
-        {
-            foreach (var element in PhantasmaChains)
-            {
-                if (element.Address == address) return element.Name;
-            }
-
-            return string.Empty;
-        }
-
         public async Task<SendRawTx> RegisterName(KeyPair keyPair, string name)
         {
             try
@@ -425,7 +296,6 @@ namespace Phantasma.Wallet.Controllers
             }
         }
         private List<Token> _phantasmaTokens;
-        #endregion
 
         private List<ChainElement> GetPhantasmaChains()
         {
@@ -456,5 +326,6 @@ namespace Phantasma.Wallet.Controllers
 
             return tokens;
         }
+        #endregion
     }
 }
--- PhantasmaWallet/Helpers/Utils.cs
+++ PhantasmaWallet/Helpers/Utils.cs
@@ -1,10 +1,145 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using LunarLabs.Parser.JSON;
+using Phantasma.Blockchain.Contracts;
+using Phantasma.Blockchain.Contracts.Native;
+using Phantasma.Blockchain.Tokens;
+using Phantasma.Cryptography;
+using Phantasma.Numerics;
+using Phantasma.Wallet.DTOs;
+using Token = Phantasma.Wallet.DTOs.Token;
 
 namespace Phantasma.Wallet.Helpers
 {
     public static class Utils
     {
+        public static string GetTxDescription(AccountTx tx, List<ChainElement> phantasmaChains, List<Token> phantasmaTokens)
+        {
+            string description = null;
+
+            string senderToken = null;
+            Address senderChain = Address.FromText(tx.ChainAddress);
+            Address senderAddress = Address.Null;
+
+            string receiverToken = null;
+            Address receiverChain = Address.Null;
+            Address receiverAddress = Address.Null;
+
+            BigInteger amount = 0;
+
+            foreach (var evt in tx.Events) //todo move this
+            {
+                Blockchain.Contracts.Event nativeEvent;
+                if (evt.Data != null)
+                {
+                    nativeEvent = new Blockchain.Contracts.Event((EventKind)evt.EvtKind,
+                        Address.FromText(evt.EventAddress), evt.Data.Decode());
+                }
+                else
+                {
+                    nativeEvent =
+                        new Blockchain.Contracts.Event((EventKind)evt.EvtKind, Address.FromText(evt.EventAddress));
+                }
+
+                switch (evt.EvtKind)
+                {
+                    case EvtKind.TokenSend:
+                        {
+                            var data = nativeEvent.GetContent<TokenEventData>();
+                            amount = data.value;
+                            senderAddress = nativeEvent.Address;
+                            senderToken = (data.symbol);
+                        }
+                        break;
+
+                    case EvtKind.TokenReceive:
+                        {
+                            var data = nativeEvent.GetContent<TokenEventData>();
+                            amount = data.value;
+                            receiverAddress = nativeEvent.Address;
+                            receiverChain = data.chainAddress;
+                            receiverToken = data.symbol;
+                        }
+                        break;
+
+                    case EvtKind.TokenEscrow:
+                        {
+                            var data = nativeEvent.GetContent<TokenEventData>();
+                            amount = data.value;
+                            var amountDecimal = TokenUtils.ToDecimal(amount,
+                                phantasmaTokens.SingleOrDefault(p => p.Symbol == data.symbol).Decimals);
+                            receiverAddress = nativeEvent.Address;
+                            receiverChain = data.chainAddress;
+                            var chain = GetChainName(receiverChain.Text, phantasmaChains);
+                            description =
+                                $"{amountDecimal} {data.symbol} tokens escrowed for address {receiverAddress} in {chain}";
+                        }
+                        break;
+                    case EvtKind.AddressRegister:
+                        {
+                            var name = nativeEvent.GetContent<string>();
+                            description = $"{nativeEvent.Address} registered the name '{name}'";
+                        }
+                        break;
+
+                    case EvtKind.FriendAdd:
+                        {
+                            var address = nativeEvent.GetContent<Address>();
+                            description = $"{nativeEvent.Address} added '{address} to friends.'";
+                        }
+                        break;
+
+                    case EvtKind.FriendRemove:
+                        {
+                            var address = nativeEvent.GetContent<Address>();
+                            description = $"{nativeEvent.Address} removed '{address} from friends.'";
+                        }
+                        break;
+                }
+            }
+
+            if (description == null)
+            {
+                if (amount > 0 && senderAddress != Address.Null && receiverAddress != Address.Null &&
+                    senderToken != null && senderToken == receiverToken)
+                {
+                    var amountDecimal = TokenUtils.ToDecimal(amount,
+                        phantasmaTokens.SingleOrDefault(p => p.Symbol == senderToken).Decimals);
+                    description =
+                        $"{amountDecimal} {senderToken} sent from {senderAddress.Text} to {receiverAddress.Text}";
+                }
+                else if (amount > 0 && receiverAddress != Address.Null && receiverToken != null)
+                {
+                    var amountDecimal = TokenUtils.ToDecimal(amount,
+                        phantasmaTokens.SingleOrDefault(p => p.Symbol == receiverToken).Decimals);
+                    description = $"{amountDecimal} {receiverToken} received on {receiverAddress.Text} ";
+                }
+                else
+                {
+                    description = "Custom transaction";
+                }
+
+                if (receiverChain != Address.Null && receiverChain != senderChain)
+                {
+                    description +=
+                        $" from {GetChainName(senderChain.Text, phantasmaChains)} chain to {GetChainName(receiverChain.Text, phantasmaChains)} chain";
+                }
+            }
+
+            return description;
+        }
+
+        private static string GetChainName(string address, List<ChainElement> phantasmaChains)
+        {
+            foreach (var element in phantasmaChains)
+            {
+                if (element.Address == address) return element.Name;
+            }
+
+            return string.Empty;
+        }
+
         public static decimal GetCoinRate(uint ticker, string symbol = "USD")
         {
             var url = $"https://api.coinmarketcap.com/v2/ticker/{ticker}/?convert={symbol}";
